2c4e8f7798d66077b379a36879bb8b0c621dc788
claudrob <crobinson@tec.mx>
2022-11-30 18:19:57 -0600

    Multi Model Closes #1.

--- pet-clinic-web/src/main/java/my/edu/sfgpetclinic/web/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/my/edu/sfgpetclinic/web/SfgPetClinicApplication.java
@@ -1,4 +1,4 @@
-package my.edu.sfgpetclinic;
+package my.edu.sfgpetclinic.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
