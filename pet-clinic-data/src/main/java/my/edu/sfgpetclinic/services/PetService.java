d4d750b96bba2065eca4a65f7e95c55c9747e563
claudrob <crobinson@tec.mx>
2022-12-07 14:11:38 -0600

    Setting up services.

--- pet-clinic-data/src/main/java/my/edu/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/my/edu/sfgpetclinic/services/PetService.java
@@ -1,4 +1,14 @@
 package my.edu.sfgpetclinic.services;
 
+import my.edu.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
 public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
 }
