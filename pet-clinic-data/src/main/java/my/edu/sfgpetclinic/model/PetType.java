74abf09a5aa28c6ee0dfbffac69f19f99a8527ea
claudrob <crobinson@tec.mx>
2022-12-07 14:21:14 -0600

    Adding Base Entity. Closes #10

--- pet-clinic-data/src/main/java/my/edu/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/my/edu/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package my.edu.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
--- pet-clinic-data/src/main/java/my/edu/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/my/edu/sfgpetclinic/model/Vet.java
@@ -1,4 +1,4 @@
 package my.edu.sfgpetclinic.model;
 
-public class Vet extends Person{
+public class Vet extends Person {
 }
