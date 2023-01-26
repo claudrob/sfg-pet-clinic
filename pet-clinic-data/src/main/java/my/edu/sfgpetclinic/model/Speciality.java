ee503be6437f9c712a023d393ac385a204f96a1a
Claud Robinson <crobinson4@west.com>
2023-01-26 15:46:55 -0600

    Added Specialties Map impl. Closes #31

--- pet-clinic-data/src/main/java/my/edu/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/my/edu/sfgpetclinic/model/Speciality.java
@@ -1,6 +1,6 @@
 package my.edu.sfgpetclinic.model;
 
-public class Specialty extends BaseEntity{
+public class Speciality extends BaseEntity{
 
     private String description;
 
---
+++ pet-clinic-data/src/main/java/my/edu/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package my.edu.sfgpetclinic.services;
+
+import my.edu.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long>{
+}
